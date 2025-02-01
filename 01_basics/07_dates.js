7217f21c785861c4a62a413b136e64f5e1383af2
vkupadhyay2609 <upadhyaysaurav222@gmail.com>
2025-02-01 21:53:45 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,9 @@
+//let myDate = new Date()
+//console.log(myDate.toString());
+//console.log(myDate.toJSON());
+
+let myCreatedDate=new Date(2025,1,2)
+console.log(myCreatedDate.toDateString());
+
+let myTimeStamp= Date.now();
+console.log(myCreatedDate.getTime());
